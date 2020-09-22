4e605b19c9a4a5c0040873d7f8d4aecb3a8f40e1
Martin Cotta <mcotta@nerdwallet.com>
2020-09-21 18:24:13 -0700

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Martin Cotta on 9/21/20.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
